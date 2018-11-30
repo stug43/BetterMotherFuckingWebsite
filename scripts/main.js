8ea094b201dd210575147c11b82026a3b0d60ae6
stug43 <nganta.a@hotmail.fr>
2018-11-30 02:54:24 +0100

    ajout d'un fichier javascript générant une alerte et ajout d'un mode psychedelic -> modif des 2 fichiers déjà existants

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+function myFunc() {if(confirm("Rejoins nous sur --> xxxvidsxxx <--, Des milliers de vidéos à uploader")){window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ','message')}};
+setTimeout(myFunc,10000);
