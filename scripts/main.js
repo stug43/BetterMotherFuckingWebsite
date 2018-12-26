943467a274e8c19949f33a409e1f6dbebc48a9f2
stug43 <nganta.a@hotmail.fr>
2018-12-26 21:07:17 +0100

    bouton retour

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 function myFunc() {if(confirm("Rejoins nous sur --> xxxvidsxxx <--, Des milliers de vidéos à uploader")){window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ','message')}};
 setTimeout(myFunc,10000);
+function psychemode() {
+	if (document.body.className=='')
+		{document.body.className='psyche';}
+	else
+		{document.body.className='';}
+}
